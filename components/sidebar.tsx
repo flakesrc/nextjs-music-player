6e7f4551a9050a688516a26811b7d264a9fd70c3
Marcos Souza <flakesrc@protonmail.com>
2022-04-26 10:56:20 -0300

    add sidebar component

---
+++ components/sidebar.tsx
@@ -0,0 +1,16 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+
+const Sidebar = () => {
+  return <div>Sidebar Component</div>;
+};
+
+export default Sidebar;
